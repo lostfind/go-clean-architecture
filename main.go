21df802d25f3e76ac3e211ee5415f51286655f9d
lostfind <kpdzzang@gmail.com>
2022-07-10 11:27:54 +0900

    dependency injection by wire

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"zipcode/app/restapi"
-	"zipcode/data/repositories"
-	"zipcode/domain/usecase"
-
-	"zipcode/presentation/api"
-)
-
-var apiController api.ApiController
-
 func main() {
-	inject()
-	server := restapi.NewApiServer(apiController)
+	server := InitializeRestApi()
 	server.Router()
 	server.Run()
-}
-
-func inject() {
-	repository := repositories.NewFileRepository()
-	useCase := usecase.NewAddressFinder(repository)
-	apiController = api.NewApiController(useCase)
 }
